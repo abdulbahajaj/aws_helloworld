f1364e7ec90b4ba1570eaf59e22b5f58e55cbe22
abdulbahajaj <abdulbahajaj@gmail.com>
2021-02-21 23:06:02 -0500

    Added comments to the script

--- update_security_group.sh
+++ update_security_group.sh
@@ -3,21 +3,26 @@
 export AWS_CONFIG_FILE=/tmp/.aws_config
 export AWS_SHARED_CREDENTIALS_FILE=/tmp/.aws_creds
 
+# Set credentials
 aws configure set aws_access_key_id ${aws_access_key_id}
 aws configure set aws_secret_access_key ${default_secret_key}
 aws configure set default.region ${default_region}
 aws configure set default.output ${default_format}
 
+# Load ip-ranges, parse them and get cidr from each
 cidr_list=$(curl https://ip-ranges.atlassian.com | jq -r ".items[].cidr")
 
+# Iterate over the cidr list
 for cidr in ${cidr_list}
 do
+    # Assume that the CIDR is ipv4 and add it
     echo "ADDING ${cidr} as IPV4"
     if ! aws ec2 authorize-security-group-ingress \
 	 --group-name default \
 	 --protocol -1 \
 	 --cidr ${cidr};
     then
+	# If CIDR has failed to be added as IPV4 assume that it is IPV6 and add it
 	echo "ADDING ${cidr} as IPV6"
 	aws ec2 authorize-security-group-ingress \
 	    --group-name default \
